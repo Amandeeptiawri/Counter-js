c6fe3a691ca88cac9f3e5e1f99244a3dedf8a48b
Amandeeptiwri <amandeeptiawri@gmail.com>
2024-02-08 20:02:30 +0530

    commit message

---
+++ new.js
@@ -0,0 +1,17 @@
+let value=document.getElementById('v1');
+
+function increment(){
+    let a=parseInt(value.innerText);
+    a=a+1;
+    value.innerText=a;
+
+
+};
+
+function decrement(){
+    let b=parseInt(value.innerText);
+    b=b-1;
+    value.innerText=b;
+
+
+};
